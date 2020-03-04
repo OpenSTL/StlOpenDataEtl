b8e70330c17e97048e0399ce7fc937c0e308df8b
aramcharan <a.m.ramcharan@gmail.com>
2020-03-03 20:38:16 -0600

    Update transform_task_vacant.py
    added comments to provide context to vacantMapping variables.

--- etl/transformer/transform_task_vacant.py
+++ etl/transformer/transform_task_vacant.py
@@ -44,23 +44,28 @@ def transform_task_vacant():
         'NHD_NAME': 'TODO',
         'SITEADDR': 'TODO',
         'ZIP': 'ZIP',
-        'VacCatText': 'TODO',
+        'VacCatText': 'TODO', # All rows say Vacant Building
         'ResFullBat': 'FullBaths',
         'ResHlfBath': 'HalfBaths',
         'Bath_Total': calculateBathTotal,
         'ComGrdFlr': 'GroundFloorArea',
-        'ResStories': 'TODO', # TODO: not sure how to extract from BldgRes. There is a 'ResStoriesCode' which we may have to translate.
+        # In prcl, this is a string with the number of stories for the building e.g. 2 stories
+        'ResStories': 'TODO', # TODO: not sure how to extract from BldgRes. There is a 'ResStoriesCode' which we may have to translate. 
         'Ward10': 'Ward10',
         'ResAttic': 'Attic',
+        # Either full basement, partial basement or 0
         'ResBsmt': 'TODO', # TODO: not sure how to extract from BldgRes. I spy a BsmtType and BsmtFinishType
+        # Either brick or 0
         'ResExtWall': 'TODO', # TODO: translate ResExtWallType code
+        # brick & wood, brick & steel, brick & concrete; I would include the full text as I think buyers would want to know if it's a concrete foundation
         'ComConst': 'TODO', # TODO: BldgCom has codes like BW; our db has full text like "Brick & Wood"; which do we want?
-        'ResOccType': 'TODO', # TODO: translate ResOccType code
-        'ResGarage': 'TODO',
+        'ResOccType': 'TODO', # TODO: translate ResOccType code # one family, two family, four family, 0
+        'ResGarage': 'TODO', # 1 = present, 0 = none
         'ResCH': 'CentralHeating',
+        # finished, unfinished, dirt floor, 0
         'ResBmFin': 'TODO', # TODO: another translation one
-        'SqFt': 'TODO',
-        'Acres': 'TODO',
+        'SqFt': 'TODO', # numeric value of total house square footage
+        'Acres': 'TODO', # lot size
         # TODO: VB_decimal, NC_decimal, SL_decimal are computed in DB and rely on other fields (VB, NC, SL) that we will need to set
         'ResSalePri': 'ResSalePri',
         'Parcel': 'Parcel',
