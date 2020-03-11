adade7141835a1985f2ac7207c4b732954567087
Karen Wang <jigglepuff@users.noreply.github.com>
2020-03-10 20:37:17 -0500

    make.py: Added installation of mdbtools

--- make.py
+++ make.py
@@ -3,10 +3,25 @@
 
 # Execute pip freeze to collect our dependencies to a requirements.txt file
 import subprocess
+from sys import platform
 
 print("Executing pip install against collected requirements.")
 requirements = subprocess.Popen(
     ['pip', 'install', '-r', 'requirements.txt'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+print("Installing mdbtools...")
+if platform == "linux" or platform == "linux2":
+    # ubuntu
+    dependencies = subprocess.Popen(
+        ['sudo', 'apt-get', 'install','-y','mdbtools'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+elif platform == "darwin":
+    # OS X
+    dependencies = subprocess.Popen(
+        ['brew', 'install', 'mdbtools'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+elif platform == "win32":
+    # Windows
+    dependencies = subprocess.Popen(
+        ['brew', 'install', 'mdbtools'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 stdout, stderr = requirements.communicate()
 print(stdout.decode('utf-8'))
